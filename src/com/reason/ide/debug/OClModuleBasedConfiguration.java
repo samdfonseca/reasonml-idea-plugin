eaca2c41d7d74dbf12d1df2421bbd99fd3844abc
giraud <giraud.contact@yahoo.fr>
2018-12-19 12:07:41 +0100

    debug skeleton

--- src/com/reason/ide/debug/OClModuleBasedConfiguration.java
+++ src/com/reason/ide/debug/OClModuleBasedConfiguration.java
@@ -3,8 +3,8 @@ package com.reason.ide.debug;
 import com.intellij.execution.configurations.RunConfigurationModule;
 import com.intellij.openapi.project.Project;
 
-public class OCamlModuleBasedConfiguration extends RunConfigurationModule {
-    public OCamlModuleBasedConfiguration(Project project) {
+public class OClModuleBasedConfiguration extends RunConfigurationModule {
+    public OClModuleBasedConfiguration(Project project) {
         super(project);
     }
 }
